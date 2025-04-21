0da582a1a91fb668e38ac4eabafde1d6e293e62f
KyloPR <mmpadilla01@gmail.com>
2025-04-21 12:39:23 -0400

    Made the base for the menu of options

--- Structured Programming Proyect/Structured Programming Proyect.cpp
+++ Structured Programming Proyect/Structured Programming Proyect.cpp
@@ -1,12 +1,40 @@
-
-
 #include <iostream>
+
 using namespace std;
-int main()
-{
-    cout << "Hello World!\n";
 
-    cout << "Hola mundo cruel " << "\n";
+int main() {
+    int option;
 
+    do {
+        cout << "Enter an option (1-5): ";
+        cin >> option;
+
+        switch (option) {
+        case 1:
+            cout << "1 has been executed\n";
+            break;
+
+        case 2:
+            cout << "2 has been executed\n";
+            break;
+
+        case 3:
+            cout << "3 has been executed\n";
+            break;
+
+        case 4:
+            cout << "4 has been executed\n";
+            break;
+
+        case 5:
+            cout << "5 Thank you for using this software...\n";
+            break;
+
+        default:
+
+            cout << "Invalid option, please try again" << "\n";
+        }
+    } while (option != 5);
+
+    return 0;
 }
-
